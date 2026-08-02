4d6878f808915256266b73cf6872da3f29751d4b
Charissa Wolff <wolffcharissa@gmail.com>
2026-08-02 22:28:05 +0200

    made a new abstract class AbstractAsyncCOmmand.java, already implemented addpolygoncommand with it

--- src/main/java/org/computational_immunology/ext/ImmuNet/ui/commands/AddPolygonCommand.java
+++ src/main/java/org/computational_immunology/ext/ImmuNet/ui/commands/AddPolygonCommand.java
@@ -1,61 +1,34 @@
 package org.computational_immunology.ext.ImmuNet.ui.commands;
 
 import java.util.List;
-
-import org.computational_immunology.ext.ImmuNet.core.ImmuNetLog;
-import org.computational_immunology.ext.ImmuNet.core.Polygon;
-import org.computational_immunology.ext.ImmuNet.core.PolygonConverter;
-import org.computational_immunology.ext.ImmuNet.core.handlers.JsonDataUploadHandler;
+import java.util.function.Consumer;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import javafx.concurrent.Task;
+import org.computational_immunology.ext.ImmuNet.core.ImmuNetLog;
+import org.computational_immunology.ext.ImmuNet.core.handlers.JsonDataUploadHandler;
+
+
 import qupath.lib.objects.PathObject;
 
-public class AddPolygonCommand {
+public class AddPolygonCommand extends AbstractAsyncCommand<JSONObject> {
     private final PathObject polygon;
     private final JsonDataUploadHandler dataUploadHandler;
     private JSONObject polygonJson;
-    private Runnable onDone;
-    private Runnable onFailed;
-    private Task<JSONObject> task;
 
     public AddPolygonCommand(PathObject polygon, JsonDataUploadHandler dataUploadHandler) {
         this.polygon = polygon;
         this.dataUploadHandler = dataUploadHandler;
     }
 
-    public void build() {
-        Polygon polygonData = PolygonConverter.fromPathObject(polygon);
-        polygonJson = PolygonConverter.toJSONObject(polygonData);
-        task = new Task<>() {
-            @Override
-            protected JSONObject call() {
-                return postNewPolygon();
-            }
-        };
+    @Override
+    protected String getThreadName() {
+       return "add-polygon-" + polygon.getMetadata().get("id");
     }
 
-    public void start() {
-        task.setOnSucceeded(event -> {
-            JSONObject response = task.getValue();
-            ImmuNetLog.log("Successfully uploaded polygon with ID: " + polygon.getMetadata().get("id") + ". Server response: " + response.toString());
-            if (onDone != null) {
-                onDone.run();
-            }
-        });
-        task.setOnFailed(event -> {
-            ImmuNetLog.error("Could not upload polygon data", task.getException());
-            if (onFailed != null) onFailed.run(); 
-        });
-
-        Thread thread = new Thread(task, "upload-polygon-" + polygon.getMetadata().get("id"));
-        thread.setDaemon(true);
-        thread.start();
-    }
-
-    private JSONObject postNewPolygon() {
-        //now rest of information to get format we needed: 
+    @Override
+    protected JSONObject execute(Consumer<String> progressReporter) throws Exception {
+    //now rest of information to get format we needed: 
         try {
             JSONObject response = dataUploadHandler.uploadPolygonAnnotations(new JSONArray(List.of(polygonJson)));
             return response;
@@ -65,16 +38,5 @@ public class AddPolygonCommand {
         }
     }
 
-    public Task<JSONObject> getTask() {
-        return task;
-    }
-
-    public void setOnDone(Runnable callback) {
-        this.onDone = callback;
-    }
-
-    public void setOnFailed(Runnable callback) {
-        this.onFailed = callback;
-    }
     
 }
