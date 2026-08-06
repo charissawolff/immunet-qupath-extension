50dcc922b28007700bddbf47896dc7cbf4857994
Charissa Wolff <wolffcharissa@gmail.com>
2026-08-06 15:46:48 +0200

    fixed typo

--- src/main/java/org/computational_immunology/ext/ImmuNet/ui/commands/SlideLoadWorkflow.java
+++ src/main/java/org/computational_immunology/ext/ImmuNet/ui/commands/SlideLoadWorkflow.java
@@ -79,7 +79,7 @@ public class SlideLoadWorkflow {
         presentAnnotationsCommand.setOnDone(() -> {
             state.set(Worker.State.SUCCEEDED);
             List<AnnotationPoint> annotationPoints = presentAnnotationsCommand.getTask().getValue();
-            message.set("Fetched " + annotationPoints.size() + " annotations in " + selectedDataStore.getAnnotationPoints().size() + ". There are a total of " + selectedDataStore.getSelectedSlide().getTileMetadataList().size() + " tiles.");
+            message.set("Fetched " + annotationPoints.size() + " annotations. There are a total of " + selectedDataStore.getSelectedSlide().getTileMetadataList().size() + " tiles.");
         });
         //if failed, don't clear the datastore, as the slide is still loaded and can be used. 
         presentAnnotationsCommand.setOnFailed(() -> {
