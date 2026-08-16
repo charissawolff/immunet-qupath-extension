b5bd0f35fca0de98272ff69885896babbca4184e
Charissa Wolff <wolffcharissa@gmail.com>
2026-08-16 12:23:23 +0200

    made class for the prediction annotation for machine learning models from the backend

---
+++ src/main/java/org/computational_immunology/ext/ImmuNet/core/models/PredictionAnnotationPoint.java
@@ -0,0 +1,86 @@
+package org.computational_immunology.ext.ImmuNet.core.models;
+
+import java.util.Arrays;
+
+public class PredictionAnnotationPoint {
+
+    private final String dataset;
+    private final String slide;
+    private final String tile;
+    private final String t;
+    private final int x;
+    private final int y;
+    private final String modelName;
+    private final int[] positivity;
+    private final double[] prediction;
+
+public PredictionAnnotationPoint(String dataset, String slide, String tile, String modelName,
+                                    String t, int[] positivity, int x, int y, double[] prediction) {
+        this.dataset = dataset;
+        this.slide = slide;
+        this.tile = tile;
+        this.modelName = modelName;
+        this.t = t;
+        this.positivity = positivity;
+        this.x = x;
+        this.y = y;
+        this.prediction = prediction;
+    }
+ 
+    public String getDataset() {
+        return dataset;
+    }
+ 
+ 
+    public String getSlide() {
+        return slide;
+    }
+ 
+    public String getTile() {
+        return tile;
+    }
+ 
+    public String getModelName() {
+        return modelName;
+    }
+ 
+    public String getT() {
+        return t;
+    }
+ 
+ 
+    public int[] getPositivity() {
+        return positivity;
+    }
+ 
+    public int getX() {
+        return x;
+    }
+ 
+ 
+    public int getY() {
+        return y;
+    }
+ 
+ 
+    public double[] getPrediction() {
+        return prediction;
+    }
+ 
+ 
+    @Override
+    public String toString() {
+        return "PredictionAnnotationPoint{" +
+                "dataset='" + dataset + '\'' +
+                ", slide='" + slide + '\'' +
+                ", tile='" + tile + '\'' +
+                ", model='" + modelName + '\'' +
+                ", t='" + t + '\'' +
+                ", positivity=" + Arrays.toString(positivity) +
+                ", x=" + x +
+                ", y=" + y +
+                ", prediction=" + Arrays.toString(prediction) +
+                '}';
+    }
+}
+
