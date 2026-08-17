6479e0cd0f07f80903c38ddb2bfa9359f7eca0e5
Charissa Wolff <wolffcharissa@gmail.com>
2026-08-17 10:04:50 +0200

    added javadoc

--- src/main/java/org/computational_immunology/ext/ImmuNet/core/handlers/PagePoster.java
+++ src/main/java/org/computational_immunology/ext/ImmuNet/core/handlers/PagePoster.java
@@ -3,6 +3,18 @@ package org.computational_immunology.ext.ImmuNet.core.handlers;
 import java.io.IOException;
 import java.net.http.HttpResponse;
 
+/**
+ * Posts a payload (usually JSON) to backend api to save in database.
+ * @param <T> the type of the payload being posted.
+ */
 public interface PagePoster<T> {
+    /**
+     * POsts payload to localPath.
+     * @param localPath the api route to post to.
+     * @param payload the payload containing the data (usually in JSON format) to send.
+     * @return the raw HTTP response, with the body as a String (usually JSON)
+     * @throws IOException when the post request fails.
+     * @throws InterruptedException when user cancels the javafx task in which the posting is done.
+     */
     HttpResponse<String> postObject(String localPath, T payload) throws IOException, InterruptedException;
 }
